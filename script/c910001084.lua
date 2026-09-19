f37f72576c29308f2390b3f688440877a3a60c60
Mazorn <xavier.lebris44@gmail.com>
2026-09-19 17:00:58 +0200

    A1.0.6
    Modification de la DB afin d'amélioré la lisibilité des monstres de l'extra deck.
    Séparation du coût d'invocation pour le rendre plus clair.
    Séparation de l’échelle pendule afin de la aussi la rendre plus clair.
    
    Ajout du script pour toute les cartes de playmaker.
    Elle sont fonctionnel excepté cyberse shatter qui doit être reconstruit de 0 et qui prendras plus de temps.
    Néanmoins l'intégralité du maindeck et extra deck est fonctionel

---
+++ script/c910001084.lua
@@ -0,0 +1,36 @@
+--リンク・リスタート
+--Cyberse Shatter
+--Scripted by Mazorn
+local s,id=GetID()
+function s.initial_effect(c)
+	--negate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.discon)
+	e1:SetTarget(s.distg)
+	e1:SetOperation(s.disop)
+	c:RegisterEffect(e1)
+end
+function s.negcfilter(c)
+return c:IsFaceup() and c:IsRace(RACE_CYBERSE)
+end
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev) and re:IsHasCategory(CATEGORY_ATKCHANGE)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
